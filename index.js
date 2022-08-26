7f84ff22d36914ce266929b2a5e327a2b6d0a253
soukainna <soukaina1lafdili@gmail.com>
2022-08-26 16:28:15 +0200

    finish the logicof random number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rand1 = Math.floor(Math.random() * 6) + 1
 var rand2 = Math.floor(Math.random() * 6) + 1
+var h = document.querySelector("h1")
 console.log(rand1, rand2)
 
 const p1 = document.querySelector(".img1")
@@ -16,3 +17,13 @@ const p2 = document.querySelector(".img2")
 // }
 p1.setAttribute("src", `./images/dice${rand1}.png`)
 p2.setAttribute("src", `./images/dice${rand2}.png`)
+
+if (rand1 > rand2){
+    h.innerHTML = "🚩 Player 1 Wins!"
+}
+else if (rand1 < rand2){
+    h.innerHTML = "Player 2 Wins! 🚩"
+}
+else {
+    h.innerHTML = "Draw!"
+}
