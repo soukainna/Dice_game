a2360762238f1c60b90ee08c1961fbd0a45e420a
soukainna <soukaina1lafdili@gmail.com>
2022-08-26 16:32:14 +0200

    delete some lines

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 var rand1 = Math.floor(Math.random() * 6) + 1
 var rand2 = Math.floor(Math.random() * 6) + 1
 var h = document.querySelector("h1")
-console.log(rand1, rand2)
 
 const p1 = document.querySelector(".img1")
 const p2 = document.querySelector(".img2")
-// var tab = []
 
-// for(i = 0; i < 6; i++){
-
-//     tab[i] = document.createElement('img')
-// }
-
-// if (rand1 > rand2){
-//     p1.ap
-// }
 p1.setAttribute("src", `./images/dice${rand1}.png`)
 p2.setAttribute("src", `./images/dice${rand2}.png`)
 
